bb72af1bd26ed37e2bb5ca88877eccacf6ac54f5
fdgyhuj <gs1hwrang11@hotmail.com>
2024-05-12 05:46:10 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const linearSearch = (arr, target) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
-    }
-  }
-  return -1;
-};
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
+}
