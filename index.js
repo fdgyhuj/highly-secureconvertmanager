3979258c70335347a703e5e0b36909a2ada572a7
fdgyhuj <gs1hwrang11@hotmail.com>
2024-05-13 21:18:19 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
-  }
-  return -1;
-}
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => s.has(x));
+};
