571ac216d11a77b1a88211b4d10953a046bd41a9
fdgyhuj <gs1hwrang11@hotmail.com>
2024-05-16 16:45:23 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const factorial = (num) => {
-  if (num === 0) return 1;
-  return num * factorial(num - 1);
-};
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  for (const interval of intervals) {
+    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
+      minHeap.pop();
+    }
+    minHeap.push(interval[1]);
+  }
+  return minHeap.size();
+}
