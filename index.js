90fa69dbeebd5b6adb571b73e381718d98f423d9
fdgyhuj <gs1hwrang11@hotmail.com>
2024-05-17 20:24:35 +0700

    test project

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function minMeetingRooms(intervals) {
-  intervals.sort((a, b) => a[0] - b[0]);
-  const minHeap = new MinHeap();
-  for (const interval of intervals) {
-    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
-      minHeap.pop();
+function numIslands(grid) {
+  if (!grid || grid.length === 0) return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  let islands = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "1") {
+        islands++;
+        dfs(grid, i, j);
+      }
     }
-    minHeap.push(interval[1]);
   }
-  return minHeap.size();
+  return islands;
+}
+function dfs(grid, i, j) {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
 }
